9e1d9abb1e49dd4d02d0ba612e63773437c82073
atelierakihabara <atelierakihabara>
2016-09-05 17:35:00 +0900

    first commit

---
+++ app/src/main/java/com/example/atelierakihabara/pepper_camera_test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.atelierakihabara.pepper_camera_test;
+
+import android.graphics.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
